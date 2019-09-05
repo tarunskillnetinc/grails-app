5173546c42d94a2364d6777aade74eb62fb203fd
Chris Dawe <chris@wonderlane.co.uk>
2019-09-05 17:41:33 +0100

    WLPOS-93
    Implemented various changes suggested by SonarQube.

---
+++ src/main/groovy/uk/co/wonderlane/wlpos/BackOfficeLogger.groovy
@@ -0,0 +1,56 @@
+package uk.co.wonderlane.wlpos
+
+import uk.co.wonderlane.wlpos.interfaces.ILogging
+
+class BackOfficeLogger implements ILogging {
+
+    @Override
+    void logInfo(String s, String s1) {
+
+    }
+
+    @Override
+    void logInfo(String s, String s1, String s2) {
+
+    }
+
+    @Override
+    void logDebug(String s, String s1) {
+
+    }
+
+    @Override
+    void logDebug(String s, String s1, String s2) {
+
+    }
+
+    @Override
+    void logError(String s, String s1) {
+
+    }
+
+    @Override
+    void logError(String s, String s1, String s2) {
+
+    }
+
+    @Override
+    void logWarning(String s, String s1) {
+
+    }
+
+    @Override
+    void logWarning(String s, String s1, String s2) {
+
+    }
+
+    @Override
+    void logException(String s, String s1, Exception e) {
+
+    }
+
+    @Override
+    void logException(String s, String s1, String s2, Exception e) {
+
+    }
+}
--- src/main/groovy/uk/co/wonderlane/wlpos/BackOfficeRabbitService.groovy
+++ src/main/groovy/uk/co/wonderlane/wlpos/BackOfficeRabbitService.groovy
@@ -3,7 +3,7 @@ package uk.co.wonderlane.wlpos
 public class BackOfficeRabbitService extends RabbitService {
 
     public BackOfficeRabbitService(String host, int port, String username, String password) {
-        super(host, port, username, password, null, null);
+        super(host, port, username, password, null, null, new BackOfficeLogger()) // TODO Implement an actual BackOfficeLogger?
     }
 
     @Override
