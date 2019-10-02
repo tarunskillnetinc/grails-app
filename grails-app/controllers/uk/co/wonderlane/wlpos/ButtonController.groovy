38eef34ed79f11c1af1247549ae9b378d0646fd1
Terence Paterson <terry@wonderlane.co.uk>
2019-10-02 12:17:57 +0000

    Merged in feature/WLPOS-101 (pull request #2)
    WLPOS-101
    
    Added new button messages for layaway button options
    
    Approved-by: Chris Dawe <chris@wonderlane.co.uk>

--- grails-app/controllers/uk/co/wonderlane/wlpos/ButtonController.groovy
+++ grails-app/controllers/uk/co/wonderlane/wlpos/ButtonController.groovy
@@ -32,7 +32,7 @@ class ButtonController {
         def availableProcesses = [ProcessType.NAVIGATE_SALES, ProcessType.NAVIGATE_QUICK_SELL, ProcessType.NAVIGATE_SEARCH, ProcessType.NAVIGATE_RECEIPTS, ProcessType.NAVIGATE_MANAGER_FUNCTIONS,
                                   ProcessType.NAVIGATE_CUSTOMER_REFUSAL, ProcessType.NAVIGATE_BACK, ProcessType.NAVIGATE_REFUND, ProcessType.NAVIGATE_ADD_FLOAT, ProcessType.NAVIGATE_CASH_LIFT,
                                   ProcessType.NAVIGATE_PAID_OUT, ProcessType.NAVIGATE_TRAINING, ProcessType.NAVIGATE_CREATE_DOCKET, ProcessType.NAVIGATE_COMPLETE_DOCKET, ProcessType.NAVIGATE_DISCOUNT,
-                                  ProcessType.LOCK_TILL, ProcessType.VOID_BASKET, ProcessType.NO_SALE, ProcessType.LOG_OFF]
+                                  ProcessType.SAVE_BASKET, ProcessType.RETRIEVE_BASKET, ProcessType.LOCK_TILL, ProcessType.VOID_BASKET, ProcessType.NO_SALE, ProcessType.LOG_OFF]
 
         def availableSubPages = ButtonGrid.findAllByTypeAndRetailerIdAndStoreId(ButtonGridType.OTHER, springSecurityService.principal.retailerId, springSecurityService.principal.storeId)
         def availableTenderTypes = TenderType.values()
