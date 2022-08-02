698bdfcf179d40e4e2f47c94640ce97206f735ea
Chris Dawe <chris@wonderlane.co.uk>
2022-08-02 10:07:11 +0100

    WLPOS-983
    Commit change to ProductListService so that the BO loads!

--- grails-app/services/uk/co/wonderlane/wlpos/ProductListService.groovy
+++ grails-app/services/uk/co/wonderlane/wlpos/ProductListService.groovy
@@ -2,13 +2,28 @@ package uk.co.wonderlane.wlpos
 
 import grails.gorm.transactions.Transactional
 import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.format.DateTimeFormatter
+import uk.co.wonderlane.wlpos.dataaccess.DatabaseCredentials
+import uk.co.wonderlane.wlpos.dataaccess.MySqlDal
+import uk.co.wonderlane.wlpos.enums.ProductHistoryType
 import uk.co.wonderlane.wlpos.enums.wlim.ProductListStatus
 import uk.co.wonderlane.wlpos.enums.wlim.ProductListType
 
+import java.sql.CallableStatement
+import java.sql.Connection
+import java.sql.ResultSet
+import java.sql.Types
+
 @Transactional
-class ProductListService {
+class ProductListService extends MySqlDal {
 
     def springSecurityService
+
+    ProductListService(DatabaseCredentials databaseCredentials) {
+        super(databaseCredentials)
+    }
 
     def getCentralCounts(String searchTerm = null, int offset = 0, int max = 50, String sort = "startDate", String order = "DESC") {
         return ProductList.createCriteria().list([offset: offset, max: max, sort: sort, order: order]) {
@@ -47,6 +62,91 @@ class ProductListService {
             eq ("type", ProductListType.AD_HOC_SEL_BATCH)
             "in" ("status", [ProductListStatus.IN_PROGRESS, ProductListStatus.PARTIALLY_COMPLETE])
         }
+    }
+
+    def getScheduledBatches(DateTime effectiveDate) {
+        def results = [toBePrinted: [], toBeConfirmed: [], totalCount: 0]
+
+        Connection conn = getConnection()
+        CallableStatement cstmt = conn.prepareCall("{ call getScheduledChanges(?, ?, ?, ?, ?) }")
+
+        try {
+            // Call once for to be printed changes.
+            cstmt.setInt(1, springSecurityService.principal.retailerId)
+            cstmt.setInt(2, springSecurityService.principal.storeId)
+            cstmt.setNull(3, Types.TINYINT)
+
+            if (effectiveDate) {
+                cstmt.setString(4, effectiveDate.withTimeAtStartOfDay().toString(DATE_TIME_FORMAT))
+                cstmt.setString(5, effectiveDate.plusDays(1).withTimeAtStartOfDay().toString(DATE_TIME_FORMAT))
+            } else {
+                cstmt.setNull(4, Types.VARCHAR)
+                cstmt.setNull(5, Types.VARCHAR)
+            }
+
+            ResultSet rs = cstmt.executeQuery()
+
+            DateTimeFormatter dateFormatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").withZone(DateTimeZone.UTC)
+
+            try {
+                while (rs.next()) {
+                    results.toBePrinted.add(mapProductHistoryResult(rs, dateFormatter))
+                }
+            } finally {
+                rs.close()
+            }
+
+            // Call again for changes to be OKd if this is a two stage retailer.
+            if (springSecurityService.principal.retailer.twoStageSel) {
+                cstmt.clearParameters()
+                cstmt.setInt(1, springSecurityService.principal.retailerId)
+                cstmt.setInt(2, springSecurityService.principal.storeId)
+                cstmt.setInt(3, 1)
+
+                if (effectiveDate) {
+                    cstmt.setString(4, effectiveDate.withTimeAtStartOfDay().toString(DATE_TIME_FORMAT))
+                    cstmt.setString(5, effectiveDate.plusDays(1).withTimeAtStartOfDay().toString(DATE_TIME_FORMAT))
+                } else {
+                    cstmt.setNull(4, Types.VARCHAR)
+                    cstmt.setNull(5, Types.VARCHAR)
+                }
+
+                rs = cstmt.executeQuery()
+
+                try {
+                    while (rs.next()) {
+                        results.toBeConfirmed.add(mapProductHistoryResult(rs, dateFormatter))
+                    }
+                } finally {
+                    rs.close()
+                }
+            }
+        } finally {
+            cstmt.close()
+            conn.close()
+        }
+
+        return results
+    }
+
+    def mapProductHistoryResult(ResultSet rs, DateTimeFormatter dateFormatter) {
+        def result = [:]
+
+        result.id = rs.getInt("id")
+        result.productId = rs.getInt("productId")
+        result.storeId = rs.getInt("storeId")
+        result.updateDate = DateTime.parse(rs.getString("updateDate"), dateFormatter)
+        result.effectiveDate = DateTime.parse(rs.getString("effectiveDate"), dateFormatter)
+        result.type = ProductHistoryType.valueOf(rs.getString("type"))
+        result.priceBandId = rs.getInt("priceBandId")
+        result.field = rs. getString("field")
+        result.fromValue = rs. getString("fromValue")
+        result.toValue = rs. getString("toValue")
+        result.userId = rs.getInt("userId")
+        result.usersName = rs.getString("usersName")
+        result.productVariantId = rs.getInt("productVariantId")
+
+        return result
     }
 
     def getProductList(int id) {
