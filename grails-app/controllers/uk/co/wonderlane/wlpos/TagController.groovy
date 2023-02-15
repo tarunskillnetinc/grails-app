2671f5534ec9d07bb9e64ac59619de3b0da82026
Tim Adlem <tim.adlem@trustpayments.com>
2023-02-15 10:19:46 +0000

    Fix test by including index in test render

--- grails-app/controllers/uk/co/wonderlane/wlpos/TagController.groovy
+++ grails-app/controllers/uk/co/wonderlane/wlpos/TagController.groovy
@@ -74,7 +74,7 @@ class TagController {
         tagProduct.productVariantId = productVariantId
         tagProduct.productDescription = productDescription
 
-        render (template: "tagProductRow", model: [tagProduct: tagProduct])
+        render (template: "tagProductRow", model: [tagProduct: tagProduct], index: 1)
     }
 
     def save(SaveTagCommand cmd) {
