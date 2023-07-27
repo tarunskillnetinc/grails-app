676e0d86b45854b4e9e7ea5656e4d94c13f3a643
Kiran Babu Kagitha <kiran.kagitha@xebia.com>
2023-07-27 13:53:14 +0000

    Update doGrailsBuild.sh

--- doGrailsBuild.sh
+++ doGrailsBuild.sh
@@ -56,4 +56,4 @@ chmod +x gradlew
 
 ls /builds/securetrading-gl/retail-platform/retail-platform/cloud-office/build/libs/
 
-curl -u $2:$3 -X PUT "https://wonderlane.jfrog.io/artifactory/$repo/uk/co/wonderlane/wlpos/wlpos-back-office/$versionWithoutTimestamp/$1/wlpos-back-office-$version.jar" -T /builds/securetrading-gl/retail-platform/retail-platform/cloud-office/build/libs/build-$version.jar
+curl -u $2:$3 -X PUT "https://wonderlane.jfrog.io/artifactory/$repo/uk/co/wonderlane/wlpos/wlpos-back-office/$versionWithoutTimestamp/$1/wlpos-back-office-$version.jar" -T /builds/securetrading-gl/retail-platform/retail-platform/cloud-office/build/libs/cloud-office-$version.jar
