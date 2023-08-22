d96dbf29b357fdf0a1e5df380a9f03107d716df1
JGourlay <jon@wonderlane.co.uk>
2023-08-22 11:39:14 +0100

    Removed GSON change

--- src/main/groovy/uk/co/wonderlane/wlpos/GsonProvider.groovy
+++ src/main/groovy/uk/co/wonderlane/wlpos/GsonProvider.groovy
@@ -33,30 +33,6 @@ class GsonProvider {
                         return ISODateTimeFormat.dateTime().parseDateTime(json.getAsString()).withZone(DateTimeZone.UTC)
                     }
                 })
-                .registerTypeAdapter(Boolean.class, new JsonDeserializer<Boolean>() {
-                    @Override
-                    Boolean deserialize(JsonElement jsonElement, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-                        if (((JsonPrimitive) jsonElement).isBoolean()) {
-                            return jsonElement.getAsBoolean();
-                        }
-
-                        if (((JsonPrimitive) jsonElement).isString()) {
-                            String jsonValue = jsonElement.getAsString();
-                            if (jsonValue.equalsIgnoreCase("true")) {
-                                return true;
-                            } else if (jsonValue.equalsIgnoreCase("false")) {
-                                return false;
-                            } else {
-                                return null;
-                            }
-                        }
-
-                        int code = jsonElement.getAsInt();
-
-                        return code == 0 ? false : code == 1 ? true : null;
-                    }
-                })
-                .serializeNulls()
                 .create()
     }
 
