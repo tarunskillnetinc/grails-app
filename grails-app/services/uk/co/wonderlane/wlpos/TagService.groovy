c9e9d72b225fdbc2fa39c87fca2a15eaeb471e3b
Daniel Pike <daniel.pike@trustpayments.com>
2023-09-05 07:22:01 +0000

    CORE-1457

--- grails-app/services/uk/co/wonderlane/wlpos/TagService.groovy
+++ grails-app/services/uk/co/wonderlane/wlpos/TagService.groovy
@@ -7,13 +7,26 @@ class TagService {
 
     def springSecurityService
 
-    def getTags(String searchTerm = null, int offset = 0, int max = 50, String sort = "description", String order = "ASC") {
+    def getTags(String searchTerm = null, String searchBy = "everything", int offset = 0, int max = 50, String sort = "description", String order = "ASC") {
         return Tag.createCriteria().list([offset: offset, max: max, sort: sort, order: order]) {
             eq ("retailerId", springSecurityService.principal.retailerId)
             eq ("hidden", false)
 
             if (searchTerm) {
-                like ("description", "%$searchTerm%")
+                if (searchBy == "everything") {
+                    if (searchTerm.isNumber()) {
+                        or {
+                            sqlRestriction "cast( id AS char( 256 )) like '%${searchTerm}%'";
+                            like("description", "%$searchTerm%")
+                        }
+                    } else {
+                        like("description", "%$searchTerm%")
+                    }
+                } else if (searchBy == "description") {
+                    like("description", "%$searchTerm%")
+                } else if (searchBy == "tagId") {
+                    sqlRestriction "cast( id AS char( 256 )) like '%${searchTerm}%'"
+                }
             }
         }
     }
