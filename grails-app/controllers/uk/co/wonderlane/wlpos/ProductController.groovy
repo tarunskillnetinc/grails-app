6a6eca6a1d545673d379791ac3afc64b218ed25d
Tim Adlem <tim.adlem@trustpayments.com>
2023-09-26 13:48:04 +0100

    CORE-2707
    Remove overriding of uneditable retailerProductId product attribute

--- grails-app/controllers/uk/co/wonderlane/wlpos/ProductController.groovy
+++ grails-app/controllers/uk/co/wonderlane/wlpos/ProductController.groovy
@@ -471,7 +471,6 @@ class ProductController extends BaseController {
             product.vatPercentageOverride = editedProduct.vatPercentageOverride
             product.discreetMessage = editedProduct.discreetMessage
             product.status = editedProduct.status
-            product.retailerProductId = editedProduct.retailerProductId
 
             if (isRestrictionsChanged(editedProduct.restrictions, product.restrictions)) {
                 if (product.category != null) {
