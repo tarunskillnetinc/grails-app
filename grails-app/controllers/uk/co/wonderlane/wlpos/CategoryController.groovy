41bccba02e9c90c81a0f15f13f43bfceef012980
JGourlay <jon@wonderlane.co.uk>
2023-10-25 09:42:47 +0100

    Added handling for removing parent category when unticked

--- grails-app/controllers/uk/co/wonderlane/wlpos/CategoryController.groovy
+++ grails-app/controllers/uk/co/wonderlane/wlpos/CategoryController.groovy
@@ -108,6 +108,8 @@ class CategoryController extends BaseController {
             } else {
                 category.parentCategory = null
             }
+        } else {
+            category.parentCategory = null
         }
 
         if (category.hasErrors()) {
