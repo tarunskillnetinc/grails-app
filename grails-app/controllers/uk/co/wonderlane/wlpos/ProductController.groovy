9f5a3f3f1bd61e4586040819ecb48c16ac27a54b
Samuel Lucas <samuel.lucas@trustpayments.com>
2023-11-10 14:27:42 +0000

    fix error handling for product saving

--- grails-app/controllers/uk/co/wonderlane/wlpos/ProductController.groovy
+++ grails-app/controllers/uk/co/wonderlane/wlpos/ProductController.groovy
@@ -399,6 +399,7 @@ class ProductController extends BaseController {
 
         boolean newProduct
         boolean changeAffectsSel = false
+        boolean duplicateItemCode = false
 
         if (isRequest ? paramsMap.id && Integer.parseInt(paramsMap.id) > 0 : editedProduct.id && editedProduct.id > 0) {
             newProduct = false
@@ -412,28 +413,25 @@ class ProductController extends BaseController {
         if (newProduct) {
             changeAffectsSel = true
             if (isRequest) {
-                def duplicateItemCode = Product.countByRetailerIdAndItemCode(springSecurityService.principal.retailerId, paramsMap["itemCode"]) > 0
-                if (duplicateItemCode) {
-                    // CORE-2916 - the new Product(map) loads in the product variants from the itemCode, even if it already exists and this is a new product
-                    // so we need to manually correct this by creating a new product object without the item code.
-                    paramsMap["itemCode"] = ""
-                    paramsMap["variants[0].sku"] = null
-                    paramsMap["variants[0].retailPrice"] = null
-                    paramsMap["variants[0].costPrice"] = null
-                    paramsMap["variants[0].shelfLifeDays"] = null
-                    paramsMap["variants[0].shelfCapacity"] = null
-                    paramsMap["variants[0].minimumDisplayQuantity"] = null
-                    paramsMap["variants[0].effectiveDate"] = null
-                    paramsMap["variants[0].defaultSupplierId"] = null
-                    paramsMap["variants[0]"] = null
-                    paramsMap.remove("variants[0]")
+                if (paramsMap["itemCode"] != null || paramsMap["itemCode"].toString().trim().length() > 0) {
+                    duplicateItemCode = Product.countByRetailerIdAndItemCode(springSecurityService.principal.retailerId, paramsMap["itemCode"]) > 0
+                    if (duplicateItemCode) {
+                        // CORE-2916 - the new Product(map) loads in the product variants from the itemCode, even if it already exists and this is a new product
+                        // so we need to manually correct this by creating a new product object without the item code.
+                        paramsMap["itemCode"] = ""
+                        paramsMap["variants[0].sku"] = null
+                        paramsMap["variants[0].retailPrice"] = null
+                        paramsMap["variants[0].costPrice"] = null
+                        paramsMap["variants[0].shelfLifeDays"] = null
+                        paramsMap["variants[0].shelfCapacity"] = null
+                        paramsMap["variants[0].minimumDisplayQuantity"] = null
+                        paramsMap["variants[0].effectiveDate"] = null
+                        paramsMap["variants[0].defaultSupplierId"] = null
+                        paramsMap["variants[0]"] = null
+                        paramsMap.remove("variants[0]")
+                    }
                 }
                 product = new Product(paramsMap)
-                if (paramsMap["itemCode"] == null || paramsMap["itemCode"].toString().trim().length() == 0) {
-                    product.errors.rejectValue("itemCode", "product.itemCode.nullable.error")
-                } else if (duplicateItemCode) {
-                    product.errors.rejectValue("itemCode", "product.itemCode.validator.error")
-                }
             } else {
                 product = new Product()
                 copyProduct(editedProduct, product)
@@ -511,7 +509,14 @@ class ProductController extends BaseController {
             productVariantsList = getUpdatedProductVariantsOnSave(editedProduct, product, builder, changeAffectsSel, effectiveDate)
         }
 
-        if (!product.hasErrors() && product.validate()) {
+        product.validate()
+        if (duplicateItemCode) {
+            product.errors.rejectValue("itemCode", "product.itemCode.validator.error")
+        } else if (product.itemCode == null || product.itemCode.trim().isEmpty()) {
+            product.errors.rejectValue("itemCode", "product.itemCode.nullable.error")
+        }
+
+        if (!product.hasErrors()) {
             // Restrictions are validated as part of product.validate()
             restrictionsService.saveRestrictions(product.restrictions)
 
