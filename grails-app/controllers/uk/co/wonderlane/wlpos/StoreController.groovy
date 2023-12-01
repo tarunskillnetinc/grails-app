01a04fc76e03baad897a0652ec722ac53b836185
Chris Dawe <chris.dawe@trustpayments.com>
2023-12-01 13:59:43 +0000

    CORE-3317
    Added a refresh to the login session if you update the price band or range of the currently logged in store.

--- grails-app/controllers/uk/co/wonderlane/wlpos/StoreController.groovy
+++ grails-app/controllers/uk/co/wonderlane/wlpos/StoreController.groovy
@@ -77,6 +77,12 @@ class StoreController {
             }
 
             if (oldPriceBand != storeCommand.priceBand.id || oldProductRange != storeCommand.range.id) {
+                // If we've edited the store we're logged in as, refresh our login session so that spring security knows about our new range/price band.
+                if (springSecurityService.principal.storeNumber == store.config.storeNumber) {
+                    springSecurityService.principal.priceBand = storeCommand.priceBand
+                    springSecurityService.principal.range = storeCommand.range
+                }
+
                 flash.message = ["Store settings saved successfully.", "As the store's range or price band have changed, the store's tills need to be synced in order to receive the necessary product changes.", "Please perform this operation from the Till Connectivity page in the Monitoring menu."]
             } else {
                 flash.message = ["Store settings saved successfully."]
