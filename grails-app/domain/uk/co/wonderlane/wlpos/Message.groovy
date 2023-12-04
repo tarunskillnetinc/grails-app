4614f41b46a4d9edf1ef29cf91008b1f02401b2f
Chris Dawe <chris.dawe@trustpayments.com>
2023-12-04 16:03:34 +0000

    CORE-3331
    Fixed the fact that message was changed to be called text 3 years ago.

--- grails-app/domain/uk/co/wonderlane/wlpos/Message.groovy
+++ grails-app/domain/uk/co/wonderlane/wlpos/Message.groovy
@@ -6,42 +6,38 @@ class Message {
 
     int id
     int retailerId
-    String message
+    String text
     String retailerMessageCode
     Date startDate
     Date endDate
     boolean displayOncePerTransaction
     boolean displayOncePerItem
 
-//    Product saleProduct
-//    Product refundProduct
-
     static mapping = {
         table "message"
         version false
 
         retailerId column: "retailerId", sqlType: "tinyint"
-        message column: "`message`"
+        text column: "`text`"
         retailerMessageCode column: "retailerMessageCode"
         startDate column: "startDate"
         endDate column: "endDate"
         displayOncePerTransaction column: "displayOncePerTransaction"
         displayOncePerItem column: "displayOncePerItem"
-
-//        saleProduct column: "productId"
-//        refundProduct column: "productId"
     }
 
     public uk.co.wonderlane.wlpos.entities.Message getMessage() {
         uk.co.wonderlane.wlpos.entities.Message message = new uk.co.wonderlane.wlpos.entities.Message()
+
         message.setId(id)
         message.setRetailerId(retailerId)
-        message.setMessage(this.message)
+        message.setText(text)
         message.setRetailerMessageCode(retailerMessageCode)
         message.setStartDate(new DateTime(startDate))
         message.setEndDate(new DateTime(endDate))
         message.setDisplayOncePerItem(displayOncePerItem)
         message.setDisplayOncePerTransaction(displayOncePerTransaction)
+
         return message
     }
 }
