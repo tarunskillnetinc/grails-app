8994d012626b116e16214db2e599188a96cbcea6
ryan.rw <ryanrees-williams@paypoint.com>
2024-05-28 14:59:41 +0100

    WAIT-543
    Prevent duplicate products being added to product lists

--- grails-app/assets/javascripts/productList.js
+++ grails-app/assets/javascripts/productList.js
@@ -4,11 +4,20 @@ function productSelected (id, itemCode, description) {
         url: addProductUrl,
         data: { productVariantId: id },
         success: function(resp) {
-            $("#productList").append(resp);
+            let productList = $("#productList")
+
+            for (const element of productList.children()) {
+                if (element.id.toUpperCase() === "PRODUCTVARIANT" + id) {
+                    // TODO - need to display a warning somehow...
+                    return
+                }
+            }
+
+            productList.append(resp);
 
             $('#noResultsRow').hide();
 
-            let i = $('#productList').children().length - 1; // remove hidden noResultsRow
+            let i = productList.children().length - 1; // remove hidden noResultsRow
             let row = $('#productVariant' +id);
             row.addClass("wl-striped" +((i-1) % 2));
             row.find('#prod-0-id').attr("id", "prod-" + i + "-id");
