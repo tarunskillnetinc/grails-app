933ffa59efb63152ea488fd57fe6b3856fa878a2
Daniel Pike <daniel.pike@trustpayments.com>
2024-07-17 16:59:58 +0100

    WAIT-694
    Remove unused import

--- grails-app/services/uk/co/wonderlane/wlpos/ProductListService.groovy
+++ grails-app/services/uk/co/wonderlane/wlpos/ProductListService.groovy
@@ -14,7 +14,6 @@ import uk.co.wonderlane.wlpos.enums.wlim.ProductListType
 import java.sql.CallableStatement
 import java.sql.Connection
 import java.sql.ResultSet
-import java.sql.SQLException
 import java.sql.Types
 
 @Transactional
