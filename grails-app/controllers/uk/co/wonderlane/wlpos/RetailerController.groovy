6e4c8721b00cb718f1e4c7bfeb9ef3db0a4e72fc
Jason Grimes <Jason.Grimes@trustpayments.com>
2024-08-06 14:13:50 +0100

    CORE-4148
    go back to Integer as it needs to be nullable

--- grails-app/controllers/uk/co/wonderlane/wlpos/RetailerController.groovy
+++ grails-app/controllers/uk/co/wonderlane/wlpos/RetailerController.groovy
@@ -227,9 +227,9 @@ class RetailerCommand implements Validateable {
     String rabbitMqDataSyncExchange
     String rabbitMqReceiptsExchange
     String imageWebServiceUrl
-    int cfdMaxImageUpload
-    int cfdMaxProfiles
-    int cfdProfileImageCount
+    Integer cfdMaxImageUpload
+    Integer cfdMaxProfiles
+    Integer cfdProfileImageCount
 
     MultipartFile brandLogo
 
