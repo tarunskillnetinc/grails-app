b23be7011d1a83295eccdac39f63bd0a583883e8
ryan.rw <ryanrees-williams@paypoint.com>
2024-09-24 11:11:40 +0100

    WAIT-863
    Additional error rendering for Promotion Groups.

--- grails-app/controllers/uk/co/wonderlane/wlpos/PromotionController.groovy
+++ grails-app/controllers/uk/co/wonderlane/wlpos/PromotionController.groovy
@@ -705,12 +705,9 @@ class PromotionGroupCommand implements Validateable {
         tagId nullable: true, validator: { val, obj ->
             val != null || !(obj.sku == null && obj.categoryId == null)
         }
-        requiredQuantity nullable: true, range:1..999999999, validator: { val, obj ->
-            if (val != null && obj.requiredValue != null) {
-                return 'error.Promotion.requiredQuantityAndRequiredValueBothSet'
-            }
-        }
+        requiredQuantity nullable: true, range:1..999999999
         requiredValue nullable:true, min: 0.02, max:9999.99, scale: 2, validator: {val, obj ->
+            // Only one of these two fields needs a validator
             if (val != null && obj.requiredQuantity != null) {
                 return 'error.Promotion.requiredQuantityAndRequiredValueBothSet'
             }
