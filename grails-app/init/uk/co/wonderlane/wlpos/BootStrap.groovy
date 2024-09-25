6f469d2e06ffb5b76fb386678fe9e3f920e8728d
JGourlay <jon@wonderlane.co.uk>
2024-09-25 15:56:29 +0100

    Added default timezones to each envs

--- grails-app/init/uk/co/wonderlane/wlpos/BootStrap.groovy
+++ grails-app/init/uk/co/wonderlane/wlpos/BootStrap.groovy
@@ -1,8 +1,36 @@
 package uk.co.wonderlane.wlpos
 
+import grails.util.Environment
+
 class BootStrap {
+    def grailsApplication  // Inject Grails application config
 
     def init = { servletContext ->
+        switch (Environment.current.name) {
+            case 'development':
+                TimeZone.setDefault(TimeZone.getTimeZone("Pacific/Kiritimati"))  // London time
+                break
+            case 'production':
+                TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"))  // London time
+                break
+            case 'hades':
+                TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"))  // London time
+                break
+            case 'persephone':
+                TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"))  // London time
+                break
+            case 'cerberus':
+                TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"))  // London time
+                break
+            case 'zagreus':
+                TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"))  // London time
+                break
+            case 'preprod':
+                TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"))  // London time
+                break
+            default:
+                TimeZone.setDefault(TimeZone.getTimeZone("UTC"))  // Default to UTC
+        }
     }
     def destroy = {
     }
