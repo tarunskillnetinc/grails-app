4025ac32158b0631d1e66d9ce5440171eb7e9c0e
IsharaWijesinghe <ishara.wijesinghe@trustpayments.com>
2024-10-05 00:19:25 +0530

    Optimize imports

--- grails-app/services/uk/co/wonderlane/wlpos/ShiftService.groovy
+++ grails-app/services/uk/co/wonderlane/wlpos/ShiftService.groovy
@@ -13,21 +13,10 @@ import uk.co.wonderlane.wlpos.entities.cash.TenderTotal
 import uk.co.wonderlane.wlpos.entities.cashmanagement.CashManagementConfig
 import uk.co.wonderlane.wlpos.entities.transaction.FinancialWeek
 import uk.co.wonderlane.wlpos.entities.transaction.ShiftAudit
-import uk.co.wonderlane.wlpos.enums.LocationType
-import uk.co.wonderlane.wlpos.enums.ShiftAction
-import uk.co.wonderlane.wlpos.enums.ShiftStatus
-import uk.co.wonderlane.wlpos.enums.TenderMovementType
-import uk.co.wonderlane.wlpos.enums.TenderType
+import uk.co.wonderlane.wlpos.enums.*
 import uk.co.wonderlane.wlpos.reporting.Location
 
-import java.sql.CallableStatement
-import java.sql.Connection
-import java.sql.Date
-import java.sql.ResultSet
-import java.sql.SQLException
-import java.sql.Timestamp
-import java.sql.Types
-import java.time.LocalDate
+import java.sql.*
 import java.util.stream.Collectors
 
 @Transactional
