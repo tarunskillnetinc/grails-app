31eb257ae7da3d4263c1c82698613ff4a519b84f
Jason Grimes <Jason.Grimes@trustpayments.com>
2024-10-09 09:23:54 +0100

    PROD-85
    loyalty enable does not exist causing issue with repopulating products and stores.

--- grails-app/controllers/uk/co/wonderlane/wlpos/PromotionController.groovy
+++ grails-app/controllers/uk/co/wonderlane/wlpos/PromotionController.groovy
@@ -152,7 +152,6 @@ class PromotionController {
     }
 
     def save(PromotionCommand promotionCommand) {
-        def loyaltyEnable = springSecurityService.principal.retailer.config?.loyaltyRetailerConfig?.isLoyaltyEnable ? true : false
 
         // If we're logged in at a store, we want to ensure the promotionCommand contains our store.
         if (springSecurityService.principal.storeId) {
@@ -226,7 +225,7 @@ class PromotionController {
 
             redirect(action: "index")
         } else {
-            render (view: "add", model: [promotion: promotionCommand, promotionTypes: PromotionType.values(), canEdit: true, loyaltyEnable: loyaltyEnable])
+            render (view: "add", model: [promotion: promotionCommand, promotionTypes: PromotionType.values(), canEdit: true, loyaltyEnabled: springSecurityService.principal.retailer.config?.loyaltyRetailerConfig?.isLoyaltyEnabled ? true : false])
         }
     }
 
