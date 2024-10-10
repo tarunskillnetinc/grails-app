4d152f3b024fc146895cc0efa9e3ef782e026796
ryan.rw <ryanrees-williams@paypoint.com>
2024-10-10 12:02:19 +0100

    WAIT-903
    loyaltyEnable parameter used in PromotionController::add view should
    have been loyaltyEnabled instead, javascipt is now rendered completely
    and the 3 buttons in the store section continue to be responsive.

--- grails-app/controllers/uk/co/wonderlane/wlpos/PromotionController.groovy
+++ grails-app/controllers/uk/co/wonderlane/wlpos/PromotionController.groovy
@@ -151,7 +151,7 @@ class PromotionController {
     }
 
     def save(PromotionCommand promotionCommand) {
-        def loyaltyEnable = springSecurityService.principal.retailer.config?.loyaltyRetailerConfig?.isLoyaltyEnabled ? true : false
+        def loyaltyEnabled = springSecurityService.principal.retailer.config?.loyaltyRetailerConfig?.isLoyaltyEnabled ? true : false
 
         // If we're logged in at a store, we want to ensure the promotionCommand contains our store.
         if (springSecurityService.principal.storeId) {
@@ -217,7 +217,7 @@ class PromotionController {
 
             redirect(action: "index")
         } else {
-            render (view: "add", model: [promotion: promotionCommand, promotionTypes: PromotionType.values(), canEdit: true, loyaltyEnable: loyaltyEnable])
+            render (view: "add", model: [promotion: promotionCommand, promotionTypes: PromotionType.values(), canEdit: true, loyaltyEnabled: loyaltyEnabled])
         }
     }
 
