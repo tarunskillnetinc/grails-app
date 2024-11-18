5b784a819bba38e521d2c0178fa8e9f5262133ab
IsharaWijesinghe <ishara.wijesinghe@trustpayments.com>
2024-11-18 19:00:53 +0530

    STMP-517 Add uncommited file

---
+++ src/test/groovy/uk/co/wonderlane/wlpos/SafeManagementControllerSpec.groovy
@@ -0,0 +1,18 @@
+package uk.co.wonderlane.wlpos
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class SafeManagementControllerSpec extends Specification implements ControllerUnitTest<SafeManagementController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
