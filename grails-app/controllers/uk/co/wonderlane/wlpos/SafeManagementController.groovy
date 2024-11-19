18e6893739da6ebe057404feffc98550fce65044
IsharaWijesinghe <ishara.wijesinghe@trustpayments.com>
2024-11-19 10:13:46 +0530

    STMP-517 Renaming done and remove unwanted methods

--- grails-app/controllers/uk/co/wonderlane/wlpos/SafeManagementController.groovy
+++ grails-app/controllers/uk/co/wonderlane/wlpos/SafeManagementController.groovy
@@ -18,12 +18,6 @@ class SafeManagementController {
             redirect(uri: "/")
             return
         }
-
-        DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyyy")
-        String startDate = (DateTime.now(DateTimeZone.UTC).withTimeAtStartOfDay().minusDays(7)).toString(formatter)
-        String endDate = (DateTime.now(DateTimeZone.UTC).withTimeAtStartOfDay()).toString(formatter)
-
-        [startDate: params.startDate ?: startDate, endDate: params.endDate ?: endDate, safeId: params.safeId]
     }
 
     // This will load all available safe sessions and parsing to view to display
@@ -35,8 +29,8 @@ class SafeManagementController {
             //Load all safes -> Which requires to check safe type and primary status
             List<Safe> safeList = safeService.getStoreSafes()
 
-            //Check any financial week available for shifts
-            boolean isFinancialWeekExists = safeSessions.any { shift -> shift.financialWeek != null }
+            //Check any financial week available for safe session
+            boolean isFinancialWeekExists = safeSessions.any { session -> session.financialWeek != null }
 
             // Create a map of safes by their ID for quick lookup
             Map<Long, Safe> safeMap = safeList.collectEntries { [(it.id): it] }
@@ -50,13 +44,13 @@ class SafeManagementController {
                 ]
             }
 
-            render(template: "safeSessionViewerResults", model: [safeSessions : safeSessions, safeList: safeList, isFinancialWeekExists: isFinancialWeekExists, lastRefreshDate: new DateTime()])
+            render(template: "safeSessionViewerResults", model: [safeSessions : safeSessions, safeList: safeList, isFinancialWeekExists: isFinancialWeekExists])
 
         } catch (Exception ex) {
             var errorMessage = "Error loading safe session list"
             log.error("Safe session loading error error: ${ex.getMessage()}", ex)
             if (errorMessage == null || errorMessage == '') {
-                errorMessage = "Unexpected error loading shifts"
+                errorMessage = "Unexpected error loading safe sessions"
             }
             render(template: "safeSessionViewerResults", model: [errorMessage: errorMessage])
         }
