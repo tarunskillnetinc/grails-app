5d29d0a0be3368d624b222ca8d3b18cc3b7191cd
Paul Stiles <paul.stiles@trustpayments.com>
2025-01-07 10:10:57 +0000

    STMP-348: Prevent the issue where quoted strings lost their quotes when converting a List of Strings to a JSON String (product attribute list entries can now handle "null" strings and strings with backslashes)

--- grails-app/services/uk/co/wonderlane/wlpos/ProductAttributesService.groovy
+++ grails-app/services/uk/co/wonderlane/wlpos/ProductAttributesService.groovy
@@ -3,6 +3,7 @@ package uk.co.wonderlane.wlpos
 import com.google.gson.reflect.TypeToken
 import grails.gorm.transactions.Transactional
 import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
 import groovy.sql.Sql
 import uk.co.wonderlane.wlpos.dataaccess.DatabaseCredentials
 import uk.co.wonderlane.wlpos.dataaccess.MySqlDal
@@ -91,7 +92,7 @@ class ProductAttributesService extends MySqlDal{
         }
 
         try {
-            String jsonString = productAttribute.listValues
+            String jsonString = JsonOutput.toJson(productAttribute.listValues)
 
             if (jsonString) {
                 return gsonProvider.gson.fromJson(jsonString, new TypeToken<List<String>>(){}.getType())
