e0fa3cd1d9049b7d800a558135483ed4ea1f9d9f
IsharaWijesinghe <ishara.wijesinghe@trustpayments.com>
2025-01-22 20:09:42 +0530

    STMP-316 Refactoring changes done for partner category change

--- grails-app/domain/uk/co/wonderlane/wlpos/EcomSupplierCategory.groovy
+++ grails-app/domain/uk/co/wonderlane/wlpos/EcomSupplierCategory.groovy
@@ -21,8 +21,8 @@ class EcomSupplierCategory implements Serializable{
         description column: "description"
         deleted column: "deleted" , sqlType: "BIT(1)"
         ecomSupplier  column: "ecomSupplierId"
-        //ecomSupplierCategoryMappings cascade: 'all-delete-orphan', key: 'ecomSupplierCategoryId'
-        ecomSupplierCategoryMappings key: 'ecomSupplierCategoryId'
+        ecomSupplierCategoryMappings cascade: 'all-delete-orphan', key: 'ecomSupplierCategoryId'
+//        ecomSupplierCategoryMappings key: 'ecomSupplierCategoryId'
     }
 
     static constraints = {
--- grails-app/domain/uk/co/wonderlane/wlpos/EcomSupplierCategoryMapping.groovy
+++ grails-app/domain/uk/co/wonderlane/wlpos/EcomSupplierCategoryMapping.groovy
@@ -1,19 +1,23 @@
 package uk.co.wonderlane.wlpos
 
 class EcomSupplierCategoryMapping implements Serializable{
+//
+    Integer id
+    Category category
 
     static belongsTo = [ecomSupplier: EcomSupplier, ecomSupplierCategory: EcomSupplierCategory]
 
-    static hasOne = [category: Category]
+//    static hasOne = [category: Category]
 
     static mapping = {
         table "ecomsuppliercategorymapping"
         version false
 
+        id column: "id", type: "int"
         ecomSupplierCategory column: "ecomSupplierCategoryId"
         category column: 'categoryId' , fetch: 'join'
         ecomSupplier  column: "ecomSupplierId"
-        id composite: ['ecomSupplierCategory', 'ecomSupplier', 'category']
+        //id composite: ['ecomSupplierCategory', 'ecomSupplier', 'category']
     }
 
     static constraints = {
