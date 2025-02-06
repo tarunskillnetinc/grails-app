34a16707c90fde5531a82c7adc804b61201a98ba
Nishantha <nishantha.priyathilaka@trustpayments.com>
2025-02-06 16:17:29 +0530

    STMP-774 fixed the product not saving issue

--- src/main/groovy/uk/co/wonderlane/wlpos/BackOfficeLogger.groovy
+++ src/main/groovy/uk/co/wonderlane/wlpos/BackOfficeLogger.groovy
@@ -1,5 +1,7 @@
 package uk.co.wonderlane.wlpos
 
+import org.json.JSONObject
+import uk.co.wonderlane.wlpos.enums.logging.LoggingExposure
 import uk.co.wonderlane.wlpos.interfaces.ILogging
 
 class BackOfficeLogger implements ILogging {
@@ -10,7 +12,42 @@ class BackOfficeLogger implements ILogging {
     }
 
     @Override
+    void logDebug(String s, String s1, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logDebug(String s, String s1, JSONObject jsonObject) {
+
+    }
+
+    @Override
+    void logDebug(String s, String s1, JSONObject jsonObject, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
     void logDebug(String s, String s1, String s2) {
+
+    }
+
+    @Override
+    void logDebug(String s, String s1, String s2, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logDebug(String s, String s1, String s2, JSONObject jsonObject) {
+
+    }
+
+    @Override
+    void logDebug(String s, String s1, String s2, JSONObject jsonObject, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logDebug(String s, String s1, ComplexLogParams complexLogParams) {
 
     }
 
@@ -20,7 +57,42 @@ class BackOfficeLogger implements ILogging {
     }
 
     @Override
+    void logInfo(String s, String s1, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logInfo(String s, String s1, JSONObject jsonObject) {
+
+    }
+
+    @Override
+    void logInfo(String s, String s1, JSONObject jsonObject, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
     void logInfo(String s, String s1, String s2) {
+
+    }
+
+    @Override
+    void logInfo(String s, String s1, String s2, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logInfo(String s, String s1, String s2, JSONObject jsonObject) {
+
+    }
+
+    @Override
+    void logInfo(String s, String s1, String s2, JSONObject jsonObject, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logInfo(String s, String s1, ComplexLogParams complexLogParams) {
 
     }
 
@@ -30,7 +102,42 @@ class BackOfficeLogger implements ILogging {
     }
 
     @Override
+    void logWarning(String s, String s1, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logWarning(String s, String s1, JSONObject jsonObject) {
+
+    }
+
+    @Override
+    void logWarning(String s, String s1, JSONObject jsonObject, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
     void logWarning(String s, String s1, String s2) {
+
+    }
+
+    @Override
+    void logWarning(String s, String s1, String s2, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logWarning(String s, String s1, String s2, JSONObject jsonObject) {
+
+    }
+
+    @Override
+    void logWarning(String s, String s1, String s2, JSONObject jsonObject, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logWarning(String s, String s1, ComplexLogParams complexLogParams) {
 
     }
 
@@ -40,7 +147,42 @@ class BackOfficeLogger implements ILogging {
     }
 
     @Override
+    void logError(String s, String s1, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logError(String s, String s1, JSONObject jsonObject) {
+
+    }
+
+    @Override
+    void logError(String s, String s1, JSONObject jsonObject, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
     void logError(String s, String s1, String s2) {
+
+    }
+
+    @Override
+    void logError(String s, String s1, String s2, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logError(String s, String s1, String s2, JSONObject jsonObject) {
+
+    }
+
+    @Override
+    void logError(String s, String s1, String s2, JSONObject jsonObject, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logError(String s, String s1, ComplexLogParams complexLogParams) {
 
     }
 
@@ -50,7 +192,87 @@ class BackOfficeLogger implements ILogging {
     }
 
     @Override
+    void logException(String s, String s1, Throwable throwable, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logException(String s, String s1, Throwable throwable, JSONObject jsonObject) {
+
+    }
+
+    @Override
+    void logException(String s, String s1, Throwable throwable, JSONObject jsonObject, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
     void logException(String s, String s1, Throwable throwable, String s2) {
+
+    }
+
+    @Override
+    void logException(String s, String s1, Throwable throwable, String s2, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logException(String s, String s1, Throwable throwable, String s2, JSONObject jsonObject) {
+
+    }
+
+    @Override
+    void logException(String s, String s1, Throwable throwable, String s2, JSONObject jsonObject, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logException(String s, String s1, Throwable throwable, ComplexLogParams complexLogParams) {
+
+    }
+
+    @Override
+    void logAudit(String s, String s1) {
+
+    }
+
+    @Override
+    void logAudit(String s, String s1, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logAudit(String s, String s1, JSONObject jsonObject) {
+
+    }
+
+    @Override
+    void logAudit(String s, String s1, JSONObject jsonObject, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logAudit(String s, String s1, String s2) {
+
+    }
+
+    @Override
+    void logAudit(String s, String s1, String s2, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logAudit(String s, String s1, String s2, JSONObject jsonObject) {
+
+    }
+
+    @Override
+    void logAudit(String s, String s1, String s2, JSONObject jsonObject, LoggingExposure loggingExposure) {
+
+    }
+
+    @Override
+    void logAudit(String s, String s1, ComplexLogParams complexLogParams) {
 
     }
 }
