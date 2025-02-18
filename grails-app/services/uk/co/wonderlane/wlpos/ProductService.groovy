dd74e8248703da803bf38c7e69a46e97e802feaa
Tim Adlem <tim.adlem@trustpayments.com>
2025-02-18 13:40:10 +0000

    STMP-476
    Prevent removing all allergens causing a null pointer

--- grails-app/services/uk/co/wonderlane/wlpos/ProductService.groovy
+++ grails-app/services/uk/co/wonderlane/wlpos/ProductService.groovy
@@ -331,6 +331,10 @@ class ProductService extends MySqlDal {
     }
 
     def updateProductAllergens(int productId, List<Integer> allergenIds, ProductHistoryBuilder builder) {
+        if (allergenIds == null) {
+            allergenIds = new ArrayList<>()
+        }
+
         def current = ProductAllergen.getExistingProductAllergens(productId)
 
         def removed = new ArrayList<>(current)
