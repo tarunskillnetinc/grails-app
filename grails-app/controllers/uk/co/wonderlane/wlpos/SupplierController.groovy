ef56120832ec6da50e140c950bba48f880d9b7fd
Paul Stiles <paul.stiles@trustpayments.com>
2025-03-07 14:58:04 +0000

    STMP-952:  Fixes several issues:
    Search sort order now persists after deleting/reinstating a supplier, or changing indexed search page.
    Deleting or reinstating a supplier now explicitly sets the deleted state, rather than toggling the existing value
    Supplier affiliation page permissions restored

--- grails-app/controllers/uk/co/wonderlane/wlpos/SupplierController.groovy
+++ grails-app/controllers/uk/co/wonderlane/wlpos/SupplierController.groovy
@@ -26,7 +26,6 @@ class SupplierController {
     def index() {}
 
     //This is for load symbol subscription (Affiliation) view initially
-    @Secured(['ROLE_ENGINEER', 'ROLE_HEAD_OFFICE'])
     def subscriptions() {}
 
     //search for suppliers
@@ -57,7 +56,6 @@ class SupplierController {
                 ])
     }
 
-    @Secured(['ROLE_ENGINEER', 'ROLE_HEAD_OFFICE'])
     def ajaxGetSymbolGroupSubscriptions() {
         def symbolGroupSubscriptions = supplierService.getSymbolGroupSubscriptions()
         def symbolGroups = supplierService.getSymbolGroups()
@@ -93,10 +91,10 @@ class SupplierController {
     }
 
     @Secured(['ROLE_ENGINEER', 'ROLE_HEAD_OFFICE'])
-    def ajaxToggleSupplierDeletedFlag(int supplierId) {
+    def ajaxSetSupplierDeletedFlag(int supplierId, boolean supplierDeletedFlag) {
         def supplier = supplierService.getSupplier(supplierId) //Load supplier
         if (supplier != null) {
-            supplier.deleted = !supplier.deleted;
+            supplier.deleted = supplierDeletedFlag;
             supplierService.saveSupplier(supplier)
             render "OK"
         }
@@ -152,7 +150,6 @@ class SupplierController {
         }
     }
 
-    @Secured(['ROLE_ENGINEER', 'ROLE_HEAD_OFFICE'])
     def ajaxAddSymbolGroupSubscription() {
         def symbolGroups = supplierService.getSymbolGroups()
 
@@ -167,7 +164,6 @@ class SupplierController {
         render(template: "addSymbolGroupSubscription", model: [symbolGroups: symbolGroups])
     }
 
-    @Secured(['ROLE_ENGINEER', 'ROLE_HEAD_OFFICE'])
     def ajaxEditSymbolGroupSubscription(int symbolGroupSubscriptionId) {
         def symbolGroupSubscription = supplierService.getSymbolGroupSubscription(symbolGroupSubscriptionId)
 
@@ -184,7 +180,6 @@ class SupplierController {
         render(template: "addSymbolGroupSubscription", model: [symbolGroupSubscription: symbolGroupSubscription, symbolGroups: symbolGroups])
     }
 
-    @Secured(['ROLE_ENGINEER', 'ROLE_HEAD_OFFICE'])
     def ajaxSymbolGroupAction() {
         switch (Integer.parseInt(params.symbolGroupId)) {
             case 4: // Snappy
@@ -198,7 +193,6 @@ class SupplierController {
         render status: 200, text: "Sync should begin shortly for Snappy Service in Store " + springSecurityService.principal.storeNumber + "."
     }
 
-    @Secured(['ROLE_ENGINEER', 'ROLE_HEAD_OFFICE'])
     def ajaxGetSymbolGroupForm(int symbolGroupId) {
         def symbolGroupSubscription = supplierService.getSymbolGroupSubscription(symbolGroupId)
         def symbolGroups = supplierService.getSymbolGroups()
@@ -223,7 +217,6 @@ class SupplierController {
         }
     }
 
-    @Secured(['ROLE_ENGINEER', 'ROLE_HEAD_OFFICE'])
     def ajaxSaveSymbolGroupSubscription() {
         def symbolGroupSubscription
 
