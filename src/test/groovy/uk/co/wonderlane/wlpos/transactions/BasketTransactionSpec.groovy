f21932c6cce13d36302a0124fe13b53c73fb40e5
Ben Walmisley <benedict.walmisley@trustpayments.com>
2025-03-27 11:53:22 +0000

    Fixed test case failures

--- src/test/groovy/uk/co/wonderlane/wlpos/transactions/BasketTransactionSpec.groovy
+++ src/test/groovy/uk/co/wonderlane/wlpos/transactions/BasketTransactionSpec.groovy
@@ -2,8 +2,9 @@ package uk.co.wonderlane.wlpos.transactions
 
 import grails.testing.gorm.DomainUnitTest
 import spock.lang.Specification
+import uk.co.wonderlane.wlpos.entities.transactionv2.BasketTransaction
 
-class TransactionSpec extends Specification implements DomainUnitTest<Transaction> {
+class BasketTransactionSpec extends Specification implements DomainUnitTest<BasketTransaction> {
 
     def setup() {
     }
