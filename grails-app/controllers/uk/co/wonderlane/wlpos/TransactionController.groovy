0fed1567c3e33a575ce6526f98b799484808ed17
Ben Walmisley <benedict.walmisley@trustpayments.com>
2025-03-27 10:47:07 +0000

    Eliminate copypasta

--- grails-app/controllers/uk/co/wonderlane/wlpos/TransactionController.groovy
+++ grails-app/controllers/uk/co/wonderlane/wlpos/TransactionController.groovy
@@ -48,11 +48,7 @@ class TransactionController {
                     basket               : basketTransaction.basket,
                     basketItems          : basketTransaction.basket.basketItems,
                     store                : store,
-                    receipt              : receipt,
-                    containsModifiers    : receipt.receiptLines.find { it.type == ReceiptLineType.MODIFIER } ?: false,
-                    firstHorizontalLineId: receipt.receiptLines.sort { it.id }.find { it.type == ReceiptLineType.H_LINE }?.id ?: -1,
-                    maxTotalLength       : receipt.receiptLines?.findAll { it.type == ReceiptLineType.BASKET_ITEM }?.max { it.total?.toString()?.length() }?.total?.toString()?.length() ?: 0,
-                    maxVatLength: receipt.receiptLines?.findAll { it.type == ReceiptLineType.VAT_ITEM }?.max { it.total?.toString()?.length() }?.total?.toString()?.length() ?: 0
+                    receipt: receipt
             ]
         }
         catch (Exception ex) {
