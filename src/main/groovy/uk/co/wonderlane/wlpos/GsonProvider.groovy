b1e407e6c780c14f0326f39541cc404e2d20064c
Terry Paterson <terence.paterson@trustpayments.com>
2025-04-02 10:45:34 +0100

    STMP-1066
    Added missing class to GsonProvider initialisation

--- src/main/groovy/uk/co/wonderlane/wlpos/GsonProvider.groovy
+++ src/main/groovy/uk/co/wonderlane/wlpos/GsonProvider.groovy
@@ -5,7 +5,7 @@ import org.joda.time.DateTime
 import org.joda.time.DateTimeZone
 import org.joda.time.format.ISODateTimeFormat
 import uk.co.wonderlane.wlpos.entities.basketv2.BasketItem
-import uk.co.wonderlane.wlpos.entities.sns.SnsNotification
+import uk.co.wonderlane.wlpos.entities.event.message.TaskEventMessage
 import uk.co.wonderlane.wlpos.entities.transactionv2.Transaction
 import uk.co.wonderlane.wlpos.requests.clientexport.StockTransaction
 import uk.co.wonderlane.wlpos.utils.PropertyBasedInterfaceMarshal
@@ -32,7 +32,7 @@ class GsonProvider {
                     }
                 })
                 .registerTypeAdapter(StockTransaction.class, new PropertyBasedInterfaceMarshal())
-                .registerTypeAdapter(SnsNotification.class, new PropertyBasedInterfaceMarshal())
+                .registerTypeAdapter(TaskEventMessage.class, new PropertyBasedInterfaceMarshal())
                 .registerTypeAdapter(uk.co.wonderlane.wlpos.entities.basket.BasketItem.class, new PropertyBasedInterfaceMarshal())
                 .registerTypeAdapter(uk.co.wonderlane.wlpos.entities.transaction.Transaction.class, new PropertyBasedInterfaceMarshal())
                 .registerTypeAdapter(BasketItem.class, new PropertyBasedInterfaceMarshal())
