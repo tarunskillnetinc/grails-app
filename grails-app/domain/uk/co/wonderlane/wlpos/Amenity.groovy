033da8af886394abb2f9a28590c392db1374b0db
IsharaWijesinghe <ishara.wijesinghe@trustpayments.com>
2025-04-07 22:33:18 +0530

    STMP-1113 Added domain classes to CO

---
+++ grails-app/domain/uk/co/wonderlane/wlpos/Amenity.groovy
@@ -0,0 +1,21 @@
+package uk.co.wonderlane.wlpos
+
+class Amenity {
+
+    int id
+    int retailerId
+    String name
+
+    static mapping = {
+        autowire true
+        table "amenity"
+        version false
+
+        id column: "id", sqlType: "int"
+        retailerId column: "retailerId", sqlType: "tinyint"
+        name column: "name", sqlType: "varchar(40)"
+    }
+
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/uk/co/wonderlane/wlpos/StoreAmenity.groovy
@@ -0,0 +1,27 @@
+package uk.co.wonderlane.wlpos
+
+class StoreAmenity implements Serializable {
+
+   // int storeId
+//    int amenityId
+    String additionalDetail
+    int count
+    String availability
+    static belongsTo = [amenity: Amenity, store: Store]
+
+    static mapping = {
+        autowire true
+        table "storeamenity"
+        version false
+
+        id composite: ['amenity', 'store']
+        store column: "storeId", sqlType: "smallint"
+        amenity  column: "amenityId" , fetch: 'join'
+        additionalDetail column: "additionalDetail", sqlType: "varchar(60)"
+        count column: "count", sqlType: "int"
+        availability column: "availability", sqlType: "JSON"
+    }
+
+    static constraints = {
+    }
+}
